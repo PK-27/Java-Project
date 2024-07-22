a7d1c9a59c07c015fb9e38b134e994f272013b7f
Pavan Kumar <pavankumar.aryahee@gmail.com>
2024-07-22 23:18:36 +0530

    JIRA:3456-Pushing changes madeto Greater.Java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, Welcome to the project %s!", someone);
+    return String.format("Hello, Welcome to the project %s from Vijayanagaram!", someone);
   }
 }
